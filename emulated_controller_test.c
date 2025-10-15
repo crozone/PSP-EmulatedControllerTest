9a646676837f298d6af75fa5b1355adca9abe74f
Ryan Crosby <2499896+crozone@users.noreply.github.com>
2025-10-15 22:27:24 +1100

    Remove vtimer to simplify code

--- emulated_controller_test.c
+++ emulated_controller_test.c
@@ -35,12 +35,26 @@
 
 #define TIMER_PERIOD (10 * ONE_MSEC)
 
-// Maximum sleep attempts that can be blocked in a row, before sleep is allowed as a failsafe.
-#define MAX_CONSECUTIVE_SLEEP_ATTEMPTS 10
+// The controller port for which to handle input.
+// Can be either:
+// * SCE_CTRL_PORT_DS3
+// * SCE_CTRL_PORT_UNKNOWN_2
+#define CONTROLLER_PORT SCE_CTRL_PORT_DS3
+
+// The smallest offset from the analog stick's center position defining the guaranteed
+// range (center position +/- this offset) the stick returns to when being released.
+//
+// This is the same value used within the firmware to register analogue input and cancel the idle timer.
+//
+#define CTRL_ANALOG_PAD_CENTER_POS_ERROR_MARGIN (37)
+
+// The minimum amount of stick movement from center to register as a directional input.
+#define ANALOG_PAD_DIRECTION_THRESHOLD (CTRL_ANALOG_PAD_CENTER_POS_ERROR_MARGIN + 23)
+
 
 #define MODULE_NAME "EmulatedControllerTest"
 #define MAJOR_VER 1
-#define MINOR_VER 0
+#define MINOR_VER 1
 
 #define MODULE_OK       0
 #define MODULE_ERROR    1
@@ -70,9 +84,6 @@ PSP_DISABLE_NEWLIB();
 //
 // Forward declarations
 //
-static int timer_callback_handler(int unknown, int vtid, void *p_common);
-static SceUInt vtimer_handler(SceUID vtid, SceInt64 schedule, SceInt64 actual, void *p_common);
-
 static int main_thread(SceSize args, void *argp);
 static int start_main_thread(void);
 static int stop_main_thread(void);
@@ -83,78 +94,44 @@ int module_stop(SceSize args, void *argp);
 // Globals
 //
 static SceUInt g_button_state = 0;
-static SceUID g_timerCallbackId = -1;
 static SceUID g_mainThreadId = -1;
 
-// Timer Callback handler
-// int (*SceKernelCallbackFunction)(int arg1, int arg2, void *arg)
-static
-int timer_callback_handler(int unknown, int vtid, void *p_common)
-{
-    SceCtrlData pad_state;
-    if(sceCtrlPeekBufferPositive(&pad_state, 1) >= 0) {
-        int pad_x = pad_state.aX;
-        int pad_y = pad_state.aY;
-
-        SceUInt new_buttons = 0;
-
-        if(pad_x > (SCE_CTRL_ANALOG_PAD_CENTER_VALUE + 64)) {
-            new_buttons |= SCE_CTRL_RIGHT;
-        }
-
-        if(pad_x < (SCE_CTRL_ANALOG_PAD_CENTER_VALUE - 64)) {
-            new_buttons |= SCE_CTRL_LEFT;
-        }
-
-        if(pad_y > (SCE_CTRL_ANALOG_PAD_CENTER_VALUE + 64)) {
-            new_buttons |= SCE_CTRL_DOWN;
-        }
-
-        if(pad_y < (SCE_CTRL_ANALOG_PAD_CENTER_VALUE - 64)) {
-            new_buttons |= SCE_CTRL_UP;
-        }
-
-        g_button_state = new_buttons;
-    }
-    else {
-       DEBUG_PRINT("Failed to read button state!\n");
-    }
-
-    return 0;
-}
-
-// typedef unsigned int (*SceKernelVTimerHandlerWide)(SceUID vtid, unsigned long long *schedule, unsigned long long *actual, void *p_common);
-static
-SceUInt vtimer_handler(SceUID vtid, SceInt64 schedule, SceInt64 actual, void *p_common) {
-    // This handler is run in user mode, so don't call any kernel mode functions from here directly.
-    // Instead, notify a callback, which is run on the kernel callback handling thread we have set up.
-
-    // We don't know what *p_common actually is yet (it's definitely not the pointer we provided to the hander registration function).
-    // So, for now, pass the callback id to run into this hander using a global variable.
-    int cbid = g_timerCallbackId;
-    //int* cbid_ptr = (int*)p_common;
-    //int cbid = cbid_ptr != NULL ? *cbid_ptr : -1;
-    if(cbid >= 0) {
-        int callback_ret = sceKernelNotifyCallback(cbid, vtid);
-
-        if(callback_ret < 0) {
-            DEBUG_PRINT("Callback ret %d\n", callback_ret);
-            return 0;
-        }
-    }
-
-    // Return time to next invocation
-    return TIMER_PERIOD;
-}
-
 //
-// Controller callback function based on padsvc decomp
+// Controller callback function
 //
 static
 s32 ctrl_input_data_handler_func(void *pSrc, SceCtrlData2 *pDst)
 {
+    // pSrc is set up to point to g_button_state.
+    // Currently g_button_state is always 0, but it is included here for extensibility.
     SceUInt* p_new_buttons = (SceUInt*)pSrc;
     SceUInt new_buttons = p_new_buttons != NULL ? *p_new_buttons : 0;
+
+    // Demo - translate PSP analog input into DS3 directional pad buttons
+    SceCtrlData pad_state;
+    if(sceCtrlPeekBufferPositive(&pad_state, 1) >= 0) {
+        int pad_x = pad_state.aX - SCE_CTRL_ANALOG_PAD_CENTER_VALUE;
+        int pad_y = pad_state.aY - SCE_CTRL_ANALOG_PAD_CENTER_VALUE;
+
+        SceUInt direction_buttons = 0;
+        if(pad_x > ANALOG_PAD_DIRECTION_THRESHOLD) {
+            direction_buttons |= SCE_CTRL_RIGHT;
+        }
+
+        if(pad_x <= -ANALOG_PAD_DIRECTION_THRESHOLD) {
+            direction_buttons |= SCE_CTRL_LEFT;
+        }
+
+        if(pad_y > ANALOG_PAD_DIRECTION_THRESHOLD) {
+            direction_buttons |= SCE_CTRL_DOWN;
+        }
+
+        if(pad_y <= -ANALOG_PAD_DIRECTION_THRESHOLD) {
+            direction_buttons |= SCE_CTRL_UP;
+        }
+
+        new_buttons |= direction_buttons;
+    }
 
     if(new_buttons) {
         DEBUG_PRINT("Ctrl handler timestamp: 0x%08x, buttons: 0x%08x\n", pDst->timeStamp, new_buttons);
@@ -185,95 +162,54 @@ s32 ctrl_input_data_handler_func(void *pSrc, SceCtrlData2 *pDst)
 static
 int main_thread(SceSize args, void *argp)
 {
-    SceUID timer_cbid = -1;
-    SceUID vtid = -1;
-    SceUID ctrl_input_handler_id = -1;
-    int slot;
+    SceUID ctrl_input_handler_res = -1;
     int result;
 
     //
     // Setup
     //
 
-    DEBUG_PRINT("Registering controller input handler\n");
+    DEBUG_PRINT("Setting controller input handler for " str(CONTROLLER_PORT) "\n");
 
     // sceCtrl_driver_6C86AF22() enables passing through controller state from a specific external controller port buffer
-    // into the global controller buffer.
+    // into the emulation state slot with the same index as the port.
+    // This is the same emulation state set by sceCtrlSetButtonEmulation() and sceCtrlSetAnalogEmulation().
     //
-    // This allows it to be read with the basic controller read methods that return SceCtrlData, such as
-    // sceCtrlReadBufferPositive.
+    // It is effectively a thin setter function for g_ctrl.unk768.
+    // For how this enables passthrough, see: https://github.com/uofw/uofw/blob/7ca6ba13966a38667fa7c5c30a428ccd248186cf/src/kd/ctrl/ctrl.c#L1782-L1803
     //
-    // The argument is a bit field with bits set corresponding to the port.
+    // This allows the state to be read with the basic controller read functions that return SceCtrlData, such as
+    // sceCtrlReadBufferPositive, because these functions sample the emulation state.
+    //
+    // The argument is a bit field with bits set corresponding to the ports.
     // 0x01 enables SCE_CTRL_PORT_DS3 passthrough
     // 0x02 enables SCE_CTRL_PORT_UNKNOWN_2
-    // 0x00 disables passthrough such that it can only be read by the extended/extra methods that return SceCtrlData2,
+    // 0x00 disables passthrough such that it can only be read by the extended/extra functions that return SceCtrlData2,
     // such as sceCtrlReadBufferPositive2(), that takes the specific port number as an argument 
-    sceCtrl_driver_6C86AF22(SCE_CTRL_PORT_DS3);
+    sceCtrl_driver_6C86AF22(1 << (CONTROLLER_PORT - 1));
 
     // Setup sceCtrl_driver_E467BEC8() external controller port input handler.
-    // This effectively sets up an additional source for controller input polling, similar to how the DS3 controller
+    // This set the input data source for a controller port, similar to how the DS3 controller
     // is wired up internally to padsvc (Bluetooth -> DS3) on PSP Go.
     //
     // The copyInputData function is type SceCtrlInputDataTransferHandler and is called on every polling loop.
     SceCtrlInputDataTransferHandler controller_data_transfer_handler = {
+        // GUESS: unk1 is the handler structure size. This is common in many SCE handler structures.
         .unk1 = sizeof(SceCtrlInputDataTransferHandler),
         .copyInputData = ctrl_input_data_handler_func
     };
 
     // sceCtrl_driver_E467BEC8(u8 externalPort, SceCtrlInputDataTransferHandler *transferHandler, void *inputSource)
-    // The inputSource ptr is passed through into the handler function as the first argument.
-    ctrl_input_handler_id = sceCtrl_driver_E467BEC8(SCE_CTRL_PORT_DS3, &controller_data_transfer_handler, &g_button_state);
+    // The inputSource ptr is passed through into the handler function as the first argument, so it can be
+    // used as an input buffer for controller inputs.
+    ctrl_input_handler_res = sceCtrl_driver_E467BEC8(CONTROLLER_PORT, &controller_data_transfer_handler, &g_button_state);
 
-    if(ctrl_input_handler_id != SCE_ERROR_OK) {
-        DEBUG_PRINT("Failed to register controller input handler: ret 0x%08x\n", ctrl_input_handler_id);
+    if(ctrl_input_handler_res != SCE_ERROR_OK) {
+        DEBUG_PRINT("Failed to set controller input handler: ret 0x%08x\n", ctrl_input_handler_res);
     }
 
     DEBUG_PRINT("Setting controller polling mode to enable joystick\n");
     sceCtrlSetSamplingMode(SCE_CTRL_INPUT_DIGITAL_ANALOG);
-
-    DEBUG_PRINT("Creating timer callback\n");
-    timer_cbid = sceKernelCreateCallback(MODULE_NAME " Timer Callback", timer_callback_handler, NULL);
-    if(timer_cbid >= 0) {
-        // Set a global variable and reference the global variable from the handler.
-        // Hacky, but works for now.
-        g_timerCallbackId = timer_cbid;
-
-        DEBUG_PRINT("Registering timer\n");
-        vtid = sceKernelCreateVTimer(MODULE_NAME " Timer", NULL);
-
-        if(vtid >= 0) {
-            int set_vtimer_handle_ret;
-
-            // Stop the timer
-            sceKernelStopVTimer(vtid);
-
-            // Reset the timer time to zero
-            sceKernelSetVTimerTimeWide(vtid, sceKernelUSec2SysClockWide(0));
-
-            // Register callback on created vtimer.
-            // Use Wide version so we get to use SceInt64 for time instead of *SceKernelSysClock
-            set_vtimer_handle_ret = sceKernelSetVTimerHandlerWide(
-                vtid,
-                sceKernelUSec2SysClockWide(10 * ONE_MSEC),
-                vtimer_handler,
-                &g_timerCallbackId // TODO: Doesn't get passed to handler directly, the ptr changes.
-                );
-
-            if(set_vtimer_handle_ret < 0) {
-                DEBUG_PRINT("Failed to register LED timer handler: ret 0x%08x\n", set_vtimer_handle_ret);
-            }
-
-            DEBUG_PRINT("Start timer\n");
-            sceKernelStartVTimer(vtid);
-                    
-        }
-        else {
-            DEBUG_PRINT("Failed to create LED timer: ret 0x%08x\n", vtid);
-        }
-    }
-    else {
-        DEBUG_PRINT("Failed to create timer callback: ret 0x%08x\n", timer_cbid);
-    }
 
     //
     // Sleep and process callbacks until we get woken up
@@ -284,27 +220,12 @@ int main_thread(SceSize args, void *argp)
     //
     // Cleanup
     //
-    if(vtid >= 0) {
-        DEBUG_PRINT("Deleting timer\n");
-        int delete_vtimer_ret = sceKernelDeleteVTimer(vtid);
-        if(delete_vtimer_ret < 0) {
-            DEBUG_PRINT("Failed to delete timer handler: ret 0x%08x\n", delete_vtimer_ret);
-        }
-    }
+    if(ctrl_input_handler_res == SCE_ERROR_OK) {
+        DEBUG_PRINT("Unsetting controller input handler for " str(CONTROLLER_PORT) "\n");
 
-    if(timer_cbid >= 0) {
-        DEBUG_PRINT("Deleting timer callback\n");
-        result = sceKernelDeleteCallback(timer_cbid);
+        result = sceCtrl_driver_E467BEC8(CONTROLLER_PORT, NULL, NULL);
         if(result < 0) {
-            DEBUG_PRINT("Failed to delete timer callback: ret 0x%08x\n", result);
-        }
-    }
-
-    if(ctrl_input_handler_id >= 0) {
-        DEBUG_PRINT("Deregistering controller input handler\n");
-        result = sceCtrl_driver_E467BEC8(SCE_CTRL_PORT_DS3, NULL, NULL);
-        if(result < 0) {
-            DEBUG_PRINT("Failed to deregister controller input handler: ret 0x%08x\n", result);
+            DEBUG_PRINT("Failed to unset controller input handler: ret 0x%08x\n", result);
         }
     }
 
